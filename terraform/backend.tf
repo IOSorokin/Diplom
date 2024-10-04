355d9fc31b8cb666711d6fe4c41762c8e0929de0
IOSorokin <148979909+IOSorokin@users.noreply.github.com>
2024-10-04 11:38:56 +0300

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,8 +4,6 @@ terraform {
     bucket = "forstate2024"
     region = "ru-central1"
     key = "forstate2024/terraform.tfstate"
-#    access_key = "YCAJEk0WEkgcqfnS9NbLx7QZI"
-#    secret_key = "YCNY1ziiTS0WAkovEfvpNWH5-iohxe5jIz4_sVp9"
     skip_region_validation = true
     skip_credentials_validation = true
   }
